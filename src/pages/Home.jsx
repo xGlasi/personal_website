bbd323a99a9f4669926c5e11c54a92b274f91a11
Andreas Glashauser <andreasg@andreasg.fritz.box>
2024-03-12 20:27:03 +0100

    CHANGES

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,7 @@ import CSharpCodeTypewriter from "./CSharpCodeTypeWriter";
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+
 console.log('Supabase URL:', process.env.REACT_APP_SUPABASE_URL);
 console.log('Supabase Anon Key:', process.env.REACT_APP_SUPABASE_ANON_KEY);
 
