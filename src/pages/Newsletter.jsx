6d8f19c8ddf86447e54440c55f798559c6789e2b
Andreas Glashauser <andreasg@andreasg.fritz.box>
2024-03-16 23:02:06 +0100

    CHANGES

--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,35 +1,45 @@
 import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import { sendWelcomeEmail } from '';
+
 
 export default function Newsletter() {
-  console.log("Jetzt bisd do jawoi")
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+  const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
+  
+  const supabase = createClient(supabaseUrl, supabaseAnonKey);
+  
   const [email, setEmail] = useState('');
 
-  // Eine neue Funktion, um das Formular zu verarbeiten
   const handleSubmit = async (e) => {
-    e.preventDefault(); // Verhindert das Neuladen der Seite
-    const success = await sendWelcomeEmail(email); // Ruft die E-Mail-Versandfunktion auf
-    if (success) {
-      alert('Vielen Dank für die Anmeldung!');
-      setEmail(''); // Setzt das E-Mail-Feld zurück
-    } else {
-      alert('Es gab ein Problem mit Ihrer Anmeldung. Bitte versuchen Sie es später erneut.');
+    e.preventDefault();
+
+    // Speichern der E-Mail in Supabase
+    const { data, error } = await supabase
+      .from('newsletter_subscriber')
+      .insert([{ email }]); // Die E-Mail-Adresse wird in der Tabelle gespeichert
+
+    if (error) {
+      alert('Es gab ein Problem beim Speichern Ihrer E-Mail. Bitte versuchen Sie es später erneut.');
+      console.error('Fehler beim Speichern in Supabase:', error);
+      return;
     }
+
+    const { d, e } = await supabase.functions.invoke('send-welcome-email', {
+      body: JSON.stringify({ email: emailAddress })
+    });
   };
 
   return (
-    <div>
-      asdfsdf
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Ihre E-Mail Adresse"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <button type="submit">Anmelden</button>
-      </form>
-    </div>
-
+    <form onSubmit={handleSubmit}>
+      <input
+        type="email"
+        placeholder="Ihre E-Mail Adresse"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <button type="submit">Anmelden</button>
+    </form>
   );
 };
