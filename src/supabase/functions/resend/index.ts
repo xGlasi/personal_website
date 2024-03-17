1055a0adb2c0f1231723dae516e76f53280a0ebb
Andreas Glashauser <andreasg@andreasg.fritz.box>
2024-03-17 02:34:48 +0100

    EVERYTHING WORKS VERY COOL

--- src/supabase/functions/resend/index.ts
+++ src/supabase/functions/resend/index.ts
@@ -1,18 +1,24 @@
-import { Resend } from 'resend';
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
 
-const resend = new Resend('re_123456789');
-
-(async function () {
-  const { data, error } = await resend.emails.send({
-    from: 'Andreas Glashauser <onboarding@resend.dev>',
-    to: ['a.andreasglashauser@gmail.com'],
-    subject: 'Welcome!',
-    html: 'You are now subscribed to the newsletter :-)',
+serve(async (req) => {
+  console.log("JETZT GEHTS LOOOS");
+  console.log(Deno.env.get("RESEND_API_KEY"));
+  const res = await fetch("https://api.resend.com/emails", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${Deno.env.get("RESEND_API_KEY")}`,
+    },
+    body: JSON.stringify({
+      from: 'Acme <onboarding@resend.dev>',      
+      to: ['a.andreasglashauser@gmail.com'],
+      subject: "Welcome to the Club",
+      html: `Hey Andreas Glashauser we're glad to have you!`,
+    }),
   });
 
-  if (error) {
-    return console.error({ error });
-  }
-
+  const data = await res.json();
   console.log({ data });
-})();
+
+  return new Response("ok");
+});
