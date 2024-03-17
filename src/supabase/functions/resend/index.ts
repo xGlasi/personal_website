8363f854371f0e3211d823915f76874b0d61db1f
Andreas Glashauser <andreasg@andreasg.fritz.box>
2024-03-17 02:58:10 +0100

    CHANGES

--- src/supabase/functions/resend/index.ts
+++ src/supabase/functions/resend/index.ts
@@ -1,6 +1,21 @@
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
+import { Database } from "./types.ts";
+
+type NewsletterRecord = Database["public"]["Tables"]["newsletter_subscribers"]["Row"]
+interface WebhookPayload {
+  type: "INSERT" | "UPDATE" | "DELETE";
+  table: string;
+  record: null | NewsletterRecord;
+  schema: "public";
+  old_record: null | NewsletterRecord;
+}
 
 serve(async (req) => {
+  const payload: WebhookPayload = await req.json();
+  console.log("Lade Paylod...")
+  console.log("Neuer User:")
+  console.log(payload.record)
+
   console.log("JETZT GEHTS LOOOS");
   console.log(Deno.env.get("RESEND_API_KEY"));
   const res = await fetch("https://api.resend.com/emails", {
@@ -11,7 +26,7 @@ serve(async (req) => {
     },
     body: JSON.stringify({
       from: 'Acme <onboarding@resend.dev>',      
-      to: ['a.andreasglashauser@gmail.com'],
+      to: payload.record?.email,
       subject: "Welcome to the Club",
       html: `Hey Andreas Glashauser we're glad to have you!`,
     }),
---
+++ src/supabase/functions/resend/types.ts
@@ -0,0 +1,154 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export type Database = {
+  public: {
+    Tables: {
+      newsletter_subscribers: {
+        Row: {
+          email: string
+          id: number
+        }
+        Insert: {
+          email: string
+          id?: number
+        }
+        Update: {
+          email?: string
+          id?: number
+        }
+        Relationships: []
+      }
+      posts: {
+        Row: {
+          content: string | null
+          cover_image_url: string | null
+          excerpt: string | null
+          id: number
+          published_at: string | null
+          slug: string
+          tags: string[] | null
+          title: string
+        }
+        Insert: {
+          content?: string | null
+          cover_image_url?: string | null
+          excerpt?: string | null
+          id?: number
+          published_at?: string | null
+          slug: string
+          tags?: string[] | null
+          title: string
+        }
+        Update: {
+          content?: string | null
+          cover_image_url?: string | null
+          excerpt?: string | null
+          id?: number
+          published_at?: string | null
+          slug?: string
+          tags?: string[] | null
+          title?: string
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
+export type Tables<
+  PublicTableNameOrOptions extends
+    | keyof (Database["public"]["Tables"] & Database["public"]["Views"])
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+        Database[PublicTableNameOrOptions["schema"]]["Views"])
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
+      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
+
+export type TablesInsert<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
+
+export type TablesUpdate<
+  PublicTableNameOrOptions extends
+    | keyof Database["public"]["Tables"]
+    | { schema: keyof Database },
+  TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
+    : never = never
+> = PublicTableNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
+
+export type Enums<
+  PublicEnumNameOrOptions extends
+    | keyof Database["public"]["Enums"]
+    | { schema: keyof Database },
+  EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
+    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
+    : never = never
+> = PublicEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
